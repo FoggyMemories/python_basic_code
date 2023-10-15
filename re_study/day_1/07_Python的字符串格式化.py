41e79916ae4b79a1b0045ad6838021869ac78b62
QianRui <diiioman@foxmail.com>
2023-10-15 23:35:14 +0800

    INIT

---
+++ re_study/day_1/07_Python的字符串格式化.py
@@ -0,0 +1,10 @@
+# 通过占位的形式,完成拼接
+name = "钱睿"
+message = "是神!%s" % name
+print(message)
+
+# 通过占位的形式,完成数字和字符串的拼接
+class_num = 57
+avg_salary = 156798
+message = "大数据毕业,上海%s,平均工资:%s" % (class_num, avg_salary)
+print(message)
