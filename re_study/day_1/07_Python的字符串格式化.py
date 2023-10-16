38e378cf616800077c5ff7029fd4b2040acf7d1e
QianRui <diiioman@foxmail.com>
2023-10-16 16:25:09 +0800

    Python的字符串格式化

--- re_study/day_1/07_Python的字符串格式化.py
+++ re_study/day_1/07_Python的字符串格式化.py
@@ -8,3 +8,11 @@ class_num = 57
 avg_salary = 156798
 message = "大数据毕业,上海%s,平均工资:%s" % (class_num, avg_salary)
 print(message)
+
+num1 = 11
+num2 = 11.345
+
+print("数字11宽度限制5，结果是：%5d" % num1)
+print("数字11宽度限制1，结果是：%1d" % num1)
+print("数字11.345宽度限制17,小数精度2，结果是：%7.2f" % num2)
+print("数字11.345不限制宽度,小数精度2，结果是：%.2f" % num2)
